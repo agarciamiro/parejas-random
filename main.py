8286d768508d96954c91b6fdb120966354dfc289
agarciamiro <alfonso.garciamiro@ipn.com.pe>
2025-12-02 03:01:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-rom fastapi import FastAPI, Request
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from logic import generar_parejas
