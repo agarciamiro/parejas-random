fd76821cb0c09eae04234b7ccc4790a612e9416e
agarciamiro <alfonso.garciamiro@ipn.com.pe>
2025-12-02 02:20:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
-from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 from logic import generar_parejas
 
-app = FastAPI(title="Parejas Random API")
+app = FastAPI()
 
 # Servir carpeta /static
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# Servir templates
+templates = Jinja2Templates(directory="templates")
+
+
+# -------- MODELO DEL ENDPOINT --------
 class Personas(BaseModel):
     nombres: list[str]
+
 
 @app.post("/generar")
 def generar(personas: Personas):
     return generar_parejas(personas.nombres)
 
-# Ruta raíz "/" → mostrar frontend
-@app.get("/")
-def read_root():
-    return FileResponse("static/index.html")
+
+# -------- RUTA PRINCIPAL "/" --------
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
