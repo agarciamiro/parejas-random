3d6111fb32edd0d98cffa62baaa6df7822295cbc
agarciamiro <alfonso.garciamiro@ipn.com.pe>
2025-12-02 06:00:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,18 +6,20 @@ from logic import generar_parejas
 
 app = FastAPI()
 
-# Templates y archivos estáticos
+# Carpetas
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 # Página principal
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+
 # API
 @app.post("/generar")
 def generar(data: dict):
     nombres = data.get("nombres", [])
-    return generar_parejas(nombres)
+
 
