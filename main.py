7a9da3a4c48d837f1b520fd6342f58330c7f8b22
agarciamiro <alfonso.garciamiro@ipn.com.pe>
2025-12-02 05:36:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from logic import generar_parejas
 
 app = FastAPI()
 
-# RUTAS DE TEMPLATES Y STATIC
+# Rutas de TEMPLATES / STATIC
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# SERVIR LA PÁGINA PRINCIPAL
+
+# Página principal
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-# API PARA GENERAR LAS PAREJAS
+
+# API JSON
 @app.post("/generar")
 def generar(data: dict):
     nombres = data.get("nombres", [])
